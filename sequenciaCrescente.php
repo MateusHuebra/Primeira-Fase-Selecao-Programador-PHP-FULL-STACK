ba778f7cf815b88cf4c81c28a8057262e9a9fe56
MateusHuebra <mateushuebra@gmail.com>
2021-12-14 15:04:25 -0300

    adicionado validacao de resultados da tarefa 4

--- sequenciaCrescente.php
+++ sequenciaCrescente.php
@@ -27,28 +27,32 @@ function sequenciaCrescente(array $array) {
 }
 
 $arrays = [
-    [1, 3, 2, 1],  
-    [1, 3, 2],  
-    [1, 2, 1, 2],  
-    [3, 6, 5, 8, 10, 20, 15], 
-    [1, 1, 2, 3, 4, 4], 
-    [1, 4, 10, 4, 2], 
-    [10, 1, 2, 3, 4, 5], 
-    [1, 1, 1, 2, 3], 
-    [0, -2, 5, 6], 
-    [1, 2, 3, 4, 5, 3, 5, 6], 
-    [40, 50, 60, 10, 20, 30], 
-    [1, 1], 
-    [1, 2, 5, 3, 5], 
-    [1, 2, 5, 5, 5], 
-    [10, 1, 2, 3, 4, 5, 6, 1], 
-    [1, 2, 3, 4, 3, 6], 
-    [1, 2, 3, 4, 99, 5, 6], 
-    [123, -17, -5, 1, 2, 3, 12, 43, 45], 
-    [3, 5, 67, 98, 3] 
+    [[1, 3, 2, 1], false],  
+    [[1, 3, 2], true],  
+    [[1, 2, 1, 2], false],  
+    [[3, 6, 5, 8, 10, 20, 15], false], 
+    [[1, 1, 2, 3, 4, 4], false], 
+    [[1, 4, 10, 4, 2], false], 
+    [[10, 1, 2, 3, 4, 5], true], 
+    [[1, 1, 1, 2, 3], false], 
+    [[0, -2, 5, 6], true], 
+    [[1, 2, 3, 4, 5, 3, 5, 6], false], 
+    [[40, 50, 60, 10, 20, 30], false], 
+    [[1, 1], true], 
+    [[1, 2, 5, 3, 5], true], 
+    [[1, 2, 5, 5, 5], false], 
+    [[10, 1, 2, 3, 4, 5, 6, 1], false], 
+    [[1, 2, 3, 4, 3, 6], true], 
+    [[1, 2, 3, 4, 99, 5, 6], true], 
+    [[123, -17, -5, 1, 2, 3, 12, 43, 45], true], 
+    [[3, 5, 67, 98, 3], true] 
 ];
 
 for($i=0; $i<count($arrays); $i++) {
-    echo '</br> ['.implode(', ', $arrays[$i]).'] = ';
-    echo (sequenciaCrescente($arrays[$i]))?'true':'false';
+    echo '</br> ['.implode(', ', $arrays[$i][0]).'] = > ';
+    $resultado = sequenciaCrescente($arrays[$i][0]);
+    $validacao = $resultado==$arrays[$i][1];
+    echo ($resultado)?'true':'false';
+    echo ' <span style="color: '.(($validacao)?'green':'red').'; margin: 20px;"> resultado '.(($validacao)?'válido':'inválido   ').'</span>';
+
 }
